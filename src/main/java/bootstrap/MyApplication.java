34d12cff9ec9030cd70392fb23b8b937766939b9
moham <moham@DESKTOP-AS136SM>
2019-05-30 17:25:50 +0430

    still stable hello world

--- src/main/java/bootstrap/MyApplication.java
+++ src/main/java/bootstrap/MyApplication.java
@@ -1,8 +1,11 @@
 package bootstrap;
 
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.User;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +13,21 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyApplication {
+
+    @Bean
+    InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
+
+        return new InitializingBean() {
+            public void afterPropertiesSet() throws Exception {
+
+
+                User admin = identityService.newUser("admin");
+                admin.setPassword("admin");
+                identityService.saveUser(admin);
+
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
@@ -28,6 +46,9 @@ public class MyApplication {
                 System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
                 runtimeService.startProcessInstanceByKey("oneTaskProcess");
                 System.out.println("Number of tasks after process start: " + taskService.createTaskQuery().count());
+
+
+
             }
         };
 
