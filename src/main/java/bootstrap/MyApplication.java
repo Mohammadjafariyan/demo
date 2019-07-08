c71208844667ce1b2a5ec79d1a1c909d1b885c34
Mohammad Jafariyan <mohammad@Server2.aminco2.com>
2019-07-08 13:59:47 +0430

    first branch

--- src/main/java/bootstrap/MyApplication.java
+++ src/main/java/bootstrap/MyApplication.java
@@ -3,6 +3,8 @@ package bootstrap;
 import org.activiti.engine.*;
 import org.activiti.engine.identity.User;
 import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -15,8 +17,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
-import javax.sql.DataSource;
+import java.util.Properties;
 
+import javax.sql.DataSource;
 
 /*
 @Configuration
@@ -28,36 +31,52 @@ import javax.sql.DataSource;
 @ComponentScan("controllers")
 @EnableAutoConfiguration
 public class MyApplication {
+    private static final Logger LOGGER = Logger.getLogger(MyApplication.class); // LoggerFactory.getLogger(MyApplication.class.getName());
+
     @Bean
     @Primary
     @Qualifier
-    public DataSource processEngineConfiguration() {
-        ProcessEngine processEngine = ProcessEngineConfiguration
-                .createProcessEngineConfigurationFromResource("activiti.cfg.xml").buildProcessEngine();
+    public DataSource processEngineConfiguration() throws Exception {
+        // ProcessEngine processEngine = ProcessEngineConfiguration
+        // .createProcessEngineConfigurationFromResource("activiti.cfg.xml").buildProcessEngine();
 
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(com.microsoft.sqlserver.jdbc.SQLServerDriver.class);
-        dataSource.setUrl("jdbc:sqlserver://localhost\\SQLEXPRESS:1433;database=workflow");
-        dataSource.setUsername("sa2");
-        dataSource.setPassword("sa2");
+
+        Properties properties = MyConfig.fetchProperties();
+
+        if (properties.getProperty("url") == null)
+            throw new Exception(" url is null " + properties.size());
+        String url = (String) properties.getProperty("url");
+        String username = (String) properties.getProperty("username");
+        String password = (String) properties.getProperty("password");
+        LOGGER.info("url: " + url);
+        LOGGER.info("username:" + username);
+        LOGGER.info("password" + password);
+
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        // dataSource.setUrl("jdbc:sqlserver://ATA-PC\\ATA-PCSQL2014:1433;database=workflow");
+        // dataSource.setUsername("sa");
+        // dataSource.setPassword("1");
         return dataSource;
 
-        //return processEngine;
+        // return processEngine;
     }
+
     @Bean
     InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
 
         return new InitializingBean() {
             public void afterPropertiesSet() throws Exception {
 
-
-                if(identityService.createUserQuery().userId("admin")==null){
+                if (identityService.createUserQuery().userId("admin").singleResult() == null) {
 
                     User admin = identityService.newUser("admin");
                     admin.setPassword("admin");
                     identityService.saveUser(admin);
                 }
-
 
             }
         };
@@ -69,20 +88,17 @@ public class MyApplication {
     }
 
     @Bean
-    public CommandLineRunner init(final RepositoryService repositoryService,
-                                  final RuntimeService runtimeService,
-                                  final TaskService taskService) {
+    public CommandLineRunner init(final RepositoryService repositoryService, final RuntimeService runtimeService,
+            final TaskService taskService) {
 
         return new CommandLineRunner() {
             @Override
             public void run(String... strings) throws Exception {
-                System.out.println("Number of process definitions : "
-                        + repositoryService.createProcessDefinitionQuery().count());
+                System.out.println(
+                        "Number of process definitions : " + repositoryService.createProcessDefinitionQuery().count());
                 System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
                 runtimeService.startProcessInstanceByKey("oneTaskProcess");
                 System.out.println("Number of tasks after process start: " + taskService.createTaskQuery().count());
-
-
 
             }
         };
